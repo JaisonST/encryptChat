0ec862f48e3f53315e13a95faf4543873124c511
SaniaE <sania.01@yahoo.com>
2020-12-18 10:40:09 +0400

    Added java object list

--- URLHandler/UrlTest.java
+++ URLHandler/UrlTest.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class UrlTest{
 
 	//insert ip of server here 
-	static String ip = "http://94.204./";
+	static String ip = "http://94.204.183.143/";
  
 	public static String returnVal(String call){
 		try{
@@ -26,9 +26,29 @@ public class UrlTest{
 		} 
 
 	}
+
+	public static void responseList(String json){
+		int openB =json.indexOf("{");		
+		int prevB = 0;
+		int closeB = json.indexOf("}", prevB);
+		
+		while(openB!=-1 && closeB!=-1){
+			String obj = json.substring(openB+1, closeB);
+			System.out.println(obj);
+
+			prevB = closeB;
+			openB = json.indexOf("{", prevB);
+			closeB = json.indexOf("}",openB);
+		}
+	} 
+
 	public static void main(String ar[]){
-		boolean a = login("jaisonmanu@gmail.com","123456"); 
-		System.out.println(a); 
+		//boolean a = login("jaisonmanu@gmail.com","123456"); 
+		//System.out.println(a);
+
+		String response = returnVal("test.php");
+		responseList(response);
+	 
 	}	
  
 

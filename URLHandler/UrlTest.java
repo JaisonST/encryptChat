7a2c1a5eb870612cca6f09ecb6cdd046677be70b
JaisonST <jaisonmanu@gmail.com>
2020-12-18 10:04:10 +0400

    made login Test function

--- URLHandler/UrlTest.java
+++ URLHandler/UrlTest.java
@@ -3,16 +3,32 @@ import java.io.*;
 
 public class UrlTest{
 
-	public static void main(String ar[]){
-		//insert ip of server here 
-		String ip = "http://94.204./"; 
+	//insert ip of server here 
+	static String ip = "http://94.204./";
+ 
+	public static String returnVal(String call){
 		try{
-			URL server = new URL(ip + "test.php"); 
+			URL server = new URL(ip + call); 
 			BufferedReader in =new BufferedReader(new InputStreamReader(server.openStream()));		
-			System.out.println(in.readLine());
+			return in.readLine();
 		}catch(Exception ex){
-
+			return null; 
 		} 	
+	}
+	
+	public static boolean login(String email, String password){
+		String response = returnVal("login.php?id=\""+email+"\"&key=\""+password+"\"");
+		if(response.equals("0")){
+			return false; 
+		}else{
+			System.out.println(response); 
+			return true; 
+		} 
+
+	}
+	public static void main(String ar[]){
+		boolean a = login("jaisonmanu@gmail.com","123456"); 
+		System.out.println(a); 
 	}	
  
 

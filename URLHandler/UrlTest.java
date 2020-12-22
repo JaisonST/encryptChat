5230c45cfec91a10414420819ce778cbc76933b1
JaisonST <jaisonmanu@gmail.com>
2020-12-22 15:37:36 +0400

    Created sendChat,getChats,getContacts functions

--- URLHandler/UrlTest.java
+++ URLHandler/UrlTest.java
@@ -19,7 +19,7 @@ public class UrlTest{
 		} 	
 	}
 
-	public Map<String,String> getKeyVal(String res){
+	public HashMap<String,String> getKeyVal(String res){
 		if(res.indexOf("{")==-1){
 			return null; 
 		}
@@ -29,7 +29,7 @@ public class UrlTest{
 		int valO = res.indexOf("\"", keyC+1);  
 		int valC = res.indexOf("\"", valO+1);  
 
-		Map<String,String> user = new HashMap<String,String>(); 
+		HashMap<String,String> user = new HashMap<String,String>(); 
 		while(keyO!=-1&&keyC!=-1&&valO!=-1&&valC!=-1){
 			String key = res.substring(keyO+1, keyC); 
 			String val = res.substring(valO+1, valC);
@@ -62,22 +62,59 @@ public class UrlTest{
 
 	}
 	
+	public ArrayList<HashMap<String,String>> getChats(String chatid){
+		String response = returnVal("getChats.php?chat="+chatid); 
+		ArrayList<String> l = responseList(response); 
+		ArrayList<HashMap<String,String>> cl = new ArrayList<HashMap<String,String>>();
+		if(response.equals("0")){
+			return null; 
+		}else{
+			for(String c : l){
+				HashMap<String,String> chat = getKeyVal(c); 
+				cl.add(chat);  
+			}
+			
+			return cl;  
+		} 
+		 
+	}
 
-	public void responseList(String json){
+	public ArrayList<HashMap<String,String>> getContacts(String id){
+		String response = returnVal("getContacts.php?id=\""+id+"\""); 
+		ArrayList<String> l = responseList(response); 
+		ArrayList<HashMap<String,String>> cl = new ArrayList<HashMap<String,String>>();
+		if(response.equals("0")){
+			return null; 
+		}else{
+			for(String c : l){
+				HashMap<String,String> chat = getKeyVal(c); 
+				cl.add(chat);  
+			}
+			return cl; 	
+		}
+	}
+	
+
+	public ArrayList<String> responseList(String json){
 		int openB =json.indexOf("{");		
 		int prevB = 0;
 		int closeB = json.indexOf("}", prevB);
+		ArrayList<String> m = new ArrayList<String>(); 
 		
 		while(openB!=-1 && closeB!=-1){
-			String obj = json.substring(openB+1, closeB);
-			System.out.println(obj);
-
+			String obj = json.substring(openB, closeB+1); 
+			m.add(obj);
 			prevB = closeB;
 			openB = json.indexOf("{", prevB);
 			closeB = json.indexOf("}",openB);
 		}
-	} 
 
+		return m;
+	} 
 	
+	public void sendChat(String id,String fid , String text, String chatid){
+		String response = returnVal("sendChat.php?id="+id+"&fid="+fid+"&text="+text+"&chat="+chatid); 
+	
+	} 
 
 }
